89282f0beca7746ed29c302848181cddea161e0e
epklein4 <epklein@bu.edu>
2021-08-23 18:36:55 -0400

    Added abilities

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,22 @@
 import { useState } from 'react';
 import './App.scss'
 
+import PokeSearch from './resources/PokeSearch.png'
+
 import Select from './components/Select';
 import Sprite from './components/Sprite';
+import Abilities from './components/Abilities';
 
 function App() {
   const [pokemonName, setPokemonName] = useState("pikachu")
 
   return (
     <div className="App">
+      <img className="logo" src={PokeSearch} alt=""/>
       <div className="container">
         <Select className="search" selected={setPokemonName}/>
         <Sprite pokemonName={pokemonName}/>
+        <Abilities pokemonName={pokemonName}/>
       </div>
     </div>
   );
---
+++ frontend/src/components/Abilities.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react"
+
+import queryFetch from "../utils/queryFetch"
+
+const Abilities = ({ pokemonName }: { pokemonName: string }) => {
+    const [abilities, setAbilities] = useState([""])
+
+    useEffect(() => {
+        const queryAbilities = async() => {
+            const query = await queryFetch(`
+                query {
+                    getPokemon(name: "${pokemonName}") {
+                        abilities {
+                            name
+                        }
+                    }
+                }
+            `, { name: pokemonName }
+            )
+            console.log(query.data.getPokemon.abilities)
+            setAbilities(query.data.getPokemon.abilities.map((ability: any) => {
+                return ability.name;
+            }))
+        }
+        queryAbilities()
+    }, [pokemonName])
+
+    return (
+        <div className="pokemon__abilities">
+            {abilities.map(ability => {
+                return <p className="ability" key={ability}>{ability}</p>
+            })}
+        </div>
+    )
+}
+
+export default Abilities
